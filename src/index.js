5b4d5188c13dd111b2f0aff664ad393c60730f45
Karen Bonfils <karen.bonfils@heig-vd.ch>
2023-04-04 16:08:09 +0200

    chargement des données (manque plus que données sur sakura)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import { csv } from "d3-fetch";
 
 
-//*** Traduction Kanji *********************/
+//*** Traduction Kanji *********************************************************************************/
 csv("../data/joyo_kanji.csv")
   .then(function (data) {
     // Code de la visualisation ()
@@ -13,10 +13,6 @@ csv("../data/joyo_kanji.csv")
     data.forEach((kanji) => {
       //affiche les nouveaux kanji
       kanjiListe.ecritJap.push(kanji.new);
-      //  fetch de lapi qui retourne qqch et afficher ce truc 
-
-
-
     });
     console.log(kanjiListe);
   })
@@ -25,6 +21,48 @@ csv("../data/joyo_kanji.csv")
   });
 
 
+//*** Haiku ********************************************************************************/
+csv("../data/all_haiku.csv")
+  .then(function (data) {
+    // Code de la visualisation ()
+    let haikuListe = {
+      haikus: [],
+      haijin: []
+    }
+    data.forEach((haiku) => {
+      //affiche les nouveaux haiku
+      haikuListe.haikus.push([haiku.ligne0, haiku.ligne1, haiku.ligne2]);
+      haikuListe.haijin.push(haiku.source);
+    });
+    console.log(haikuListe);
+  });
+
+
+//*** Sakura ********************************************************************************/
+
+
+
+
+
+
+
+
+
+//*** Ramen ********************************************************************************/
+csv("../data/ramen-ratings.csv")
+  .then(function (data) {
+    let ramenJapon = []
+    data.forEach((ramen) => {
+      // trier les informations et ne prendre que les ramens du Japon
+      if (ramen.Country == "Japan")
+        ramenJapon.push({
+          nom: ramen.Brand,
+          description: ramen.Variety,
+          topTen: ramen.Stars
+        });
+    });
+    console.log(ramenJapon);
+  });
 
 
 
