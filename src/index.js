86b0d2f2d2b1beb521b0174d699d5163aae77ed2
Catherine Mauron <catherine.mauron@heig-vd.ch>
2023-04-24 10:42:33 +0200

    modifications de la carte du japon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // Importation D3 (plus simple que de faire toutes les importations de ce que l'on a besoin)
 import * as d3 from "d3";
 
-
 //*** menu burger ***********************************************************************************************************************/
 d3.select("#menu").on("click", function () {
   const menu = document.querySelector("#menuDeroulant").classList;
@@ -13,9 +12,9 @@ d3.select("#menu").on("click", function () {
 });
 
 //quand il y a un mouseover, l'image #menuImg change de taille
-const rond = document.querySelector('menuImg');
-d3.select('#menuImg').on("mouseover", function () {
-  rond.size("80%")
+const rond = document.querySelector("menuImg");
+d3.select("#menuImg").on("mouseover", function () {
+  rond.size("80%");
 });
 //image reprend sa taille normale quand mouseout
 d3.select("#menuImg").on("mouseout", function () {
@@ -23,52 +22,49 @@ d3.select("#menuImg").on("mouseout", function () {
 });
 //quand on clic sur l'image, la taille devient plus petite
 d3.select("#menuImg").on("click", function () {
-  rond.size("20%")
-})
-
+  rond.size("20%");
+});
 
 //*** Haiku ******************************************************************************************************************************/
 // METTRE AFFICHAGE ALEATOIRES CHANGEANT(change tous les X temps) + BOUTON PLAY?
 
-d3.csv("../data/haiku_karen.csv")
-  .then(function (data) {
-    // Code de la visualisation ()
-    let haikuListe = {
-      haikus: [],
-      haijin: [],
-      explication: [],
-    };
-    data.forEach((haiku) => {
-      //affiche les nouveaux haiku avec un retour à la ligne à chaque ligne
-      haikuListe.haikus.push(
-        [haiku.ligne01, haiku.ligne02, haiku.ligne03].join("<br>")
-      );
-      haikuListe.haijin.push([haiku.source]); //haiku.title, : enlevé car les haikus  n'ont pas de titre
-      haikuListe.explication.push(haiku.explication);
-    });
-    // console.log(haikuListe);
-
-    //création d'un nombre random pour afficher le haiku et l'auteur en random
-    const randomHaiku = Math.floor(Math.random() * haikuListe.haikus.length);
-    //affiche le haiku et l'auteur
-    d3.select("#haiku").html(haikuListe.haikus[randomHaiku]);
-    d3.select("#titleAuthor").html(haikuListe.haijin[randomHaiku]);
-
-    //affiche la source du haiku dans un overlay quand on passe la souris sur la div #haikus
-    d3.select("#haikus").on("mouseover", function () {
-      //affiche l'overlay
-      d3.select("#overlay").style("display", "block");
-      // met l'overlay devant les autres éléments
-      d3.select("#overlay").style("z-index", "100");
-      //affiche la source du haiku
-      d3.select("#popup").html(haikuListe.explication[randomHaiku]);
-    });
-    //cache l'overlay quand on sort de la div haikus
-    d3.select("#haikus").on("mouseout", function () {
-      d3.select("#overlay").style("display", "none");
-    });
+d3.csv("../data/haiku_karen.csv").then(function (data) {
+  // Code de la visualisation ()
+  let haikuListe = {
+    haikus: [],
+    haijin: [],
+    explication: [],
+  };
+  data.forEach((haiku) => {
+    //affiche les nouveaux haiku avec un retour à la ligne à chaque ligne
+    haikuListe.haikus.push(
+      [haiku.ligne01, haiku.ligne02, haiku.ligne03].join("<br>")
+    );
+    haikuListe.haijin.push([haiku.source]); //haiku.title, : enlevé car les haikus  n'ont pas de titre
+    haikuListe.explication.push(haiku.explication);
   });
+  // console.log(haikuListe);
 
+  //création d'un nombre random pour afficher le haiku et l'auteur en random
+  const randomHaiku = Math.floor(Math.random() * haikuListe.haikus.length);
+  //affiche le haiku et l'auteur
+  d3.select("#haiku").html(haikuListe.haikus[randomHaiku]);
+  d3.select("#titleAuthor").html(haikuListe.haijin[randomHaiku]);
+
+  //affiche la source du haiku dans un overlay quand on passe la souris sur la div #haikus
+  d3.select("#haikus").on("mouseover", function () {
+    //affiche l'overlay
+    d3.select("#overlay").style("display", "block");
+    // met l'overlay devant les autres éléments
+    d3.select("#overlay").style("z-index", "100");
+    //affiche la source du haiku
+    d3.select("#popup").html(haikuListe.explication[randomHaiku]);
+  });
+  //cache l'overlay quand on sort de la div haikus
+  d3.select("#haikus").on("mouseout", function () {
+    d3.select("#overlay").style("display", "none");
+  });
+});
 
 //*** Sakura ****************************************************************************************************************************/
 //cerisiers dates floraison
@@ -82,7 +78,7 @@ d3.csv("../data/haiku_karen.csv")
 // METTRE UN BOUTON STOP/PLAY POUR POUVOIR ARRÊTER SUR UNE ANNEE
 // QUAND LA SOURIS CLIC SUR POINT AFFICHE LE NOM DU LIEU ET LA DATE PRECISE DE FLORAISON
 // DONNER UN TITRE AU GRAPHIQUE : FLoraison des cerisiers au Japon (de... à ...)
-// METTRE UNE LEGENDE AU GRAPHIQUE : (pas d idée dsl ^^') 
+// METTRE UNE LEGENDE AU GRAPHIQUE : (pas d idée dsl ^^')
 
 (async () => {
   const stockageFichiers = await Promise.all([
@@ -150,8 +146,65 @@ d3.csv("../data/haiku_karen.csv")
     }
   });
   // console.log(donnesFinales);
+  const donneeParMois = {};
+  donnesFinales.forEach((ville) => {
+    //console.log(ville);
+    //pour afficher propriétés de ville
 
-  const afficher = (data, year) => {
+    Object.keys(ville).forEach((propriete) => {
+      //Number convertit en nombre car ce sont des strings
+      // on voit qu'il y a 6 strings et cela nous permet de garder que les années
+      //console.log(Number(propriete));
+      if (!isNaN(Number(propriete))) {
+        //on décompose date en 3 partie
+        const dateParts = ville[propriete].split("-");
+        const date = `${dateParts[0]}-${dateParts[1]}`;
+        const isArray = donneeParMois[date] ? true : false;
+        const data = {
+          siteName: ville["Site Name"],
+          latitude: ville.latitude,
+          longitude: ville.longitude,
+          date: ville[propriete],
+        };
+        if (isArray) {
+          donneeParMois[date].push(data);
+        } else {
+          donneeParMois[date] = [];
+          donneeParMois[date].push(data);
+        }
+
+        //console.log(ville[propriete]);
+      }
+    });
+    //console.log(Object.keys(ville));
+  });
+  console.log(donneeParMois);
+  const toolType = document.querySelector("#toolType");
+
+  const afficheToolType = (e) => {
+    toolType.style.display = "block";
+    toolType.style.top = e.clientY + 20 + "px";
+    toolType.style.left = e.clientX + "px";
+    console.log(e);
+    toolType.innerText = e.target.__data__["Site Name"];
+
+    //console.log(e.clientY);
+  };
+  const cacherToolType = (e) => {
+    toolType.style.display = "none";
+  };
+
+  const estEclo = (data, year, month) => {
+    const dateParts = data[year].split("-");
+    const dateUn = `${dateParts[0]}-${dateParts[1]}`;
+    const dateDeux = `${year}-${month}`;
+    if (dateUn == dateDeux) {
+      return true;
+    }
+    return false;
+  };
+
+  const afficher = (data, year, month) => {
     svg
       .selectAll("circle")
       .data(data)
@@ -160,6 +213,8 @@ d3.csv("../data/haiku_karen.csv")
           enter
             .append("circle")
             //position x
+            .on("mouseover", afficheToolType)
+            .on("mouseout", cacherToolType)
             .attr("cx", (d) => {
               return projection([d.longitude, d.latitude])[0];
             })
@@ -171,7 +226,8 @@ d3.csv("../data/haiku_karen.csv")
             .attr("r", (d) => {
               //rayon de 3 si il y a de données
               //rayon de 0 s'il y a pas de données
-              return d[year] != "" ? 3 : 0;
+
+              return estEclo(d, year, month) ? 3 : 0;
             })
             .style("fill", "#fd40b1"), // Rouge du Japon : 0 100 90 0 (#e40521)
         (update) =>
@@ -186,7 +242,7 @@ d3.csv("../data/haiku_karen.csv")
               return projection([d.longitude, d.latitude])[1];
             })
             .attr("r", (d) => {
-              return d[year] != "" ? 3 : 0;
+              return estEclo(d, year, month) ? 3 : 0;
             }),
         //quand les points disparaissent
         (exit) => exit.attr("r", () => 0).remove()
@@ -194,21 +250,26 @@ d3.csv("../data/haiku_karen.csv")
   };
   const datesEvolution = document.querySelector("#datesEvolution");
 
-  let dateCourante = 1953;
-  const afficherAnnees = () => {
-    dateCourante++;
-    if (dateCourante > 2020) {
+  let anneeCourante = 1953;
+  let moisCourant = 0;
+  const afficherDate = () => {
+    moisCourant++;
+    if (anneeCourante > 2020) {
       //comme ça on a une boucle
-      dateCourante = 1953;
+      anneeCourante = 1953;
     }
-    datesEvolution.innerText = dateCourante;
+    if (moisCourant > 12) {
+      anneeCourante++;
+      moisCourant = 1;
+    }
 
-    afficher(donnesFinales, dateCourante);
+    datesEvolution.innerText = `${moisCourant} ${anneeCourante}`;
+
+    afficher(donnesFinales, anneeCourante, moisCourant);
   };
 
-  setInterval(afficherAnnees, 500);
+  setInterval(afficherDate, 500);
 })();
-
 
 //*** Traduction Kanji ******************************************************************************************************************/
 d3.csv("../data/joyo_processed.csv")
@@ -237,13 +298,18 @@ d3.csv("../data/joyo_processed.csv")
     // console.log(nombreRandom);
     //Afficher les kanjis en fonction de nombrerandom
     for (let i = 0; i < nombreRandom.length; i++) {
-      d3.select("#kanjiAleatoirs").append('p').html(kanjiListe.ecritJap[nombreRandom[i]] + " : " + kanjiListe.traduction[nombreRandom[i]]);
+      d3.select("#kanjiAleatoirs")
+        .append("p")
+        .html(
+          kanjiListe.ecritJap[nombreRandom[i]] +
+            " : " +
+            kanjiListe.traduction[nombreRandom[i]]
+        );
     }
   })
   .catch(function (error) {
     console.log(error);
   });
-
 
 //*** Ramen *****************************************************************"""""""""""""""""""""""""""""""""***************************/
 d3.csv("../data/ramen-ratings.csv").then(function (data) {
@@ -265,7 +331,7 @@ d3.csv("../data/ramen-ratings.csv").then(function (data) {
   //créer un tableau vide pour stoquer des numéros aléatoires
   const nombreRandomRamen = [];
   do {
-    //constante aléatoire 
+    //constante aléatoire
     const randomRamen = Math.floor(Math.random() * ramenJapon.length);
     //si le nombre n'est pas dans le tableau, on l'ajoute
     if (!nombreRandomRamen.includes(randomRamen)) {
@@ -279,7 +345,7 @@ d3.csv("../data/ramen-ratings.csv").then(function (data) {
   for (let i = 0; i < nombreRandomRamen.length; i++) {
     cinqRamens.push(ramenJapon[nombreRandomRamen[i]]);
   }
-  console.log(cinqRamens)
+  console.log(cinqRamens);
 
   // 3) trier ces 5 ramens par étoiles
   const triScore = cinqRamens.sort(function compare(a, b) {
@@ -292,28 +358,20 @@ d3.csv("../data/ramen-ratings.csv").then(function (data) {
 
   // 4) afficher la marque et le topFive dans l'HTML
   const affichageRamen = d3.select("#ramensAleatoirs");
-  affichageRamen.selectAll("p")
+  affichageRamen
+    .selectAll("p")
     .data(triScore)
     .enter()
     .append("p")
     .html((d) => d.marque + "  " + d.topFive);
 
   // 5) quand on clique sur un ramen, pour afficher / cacher la description en dessous du produit
-
-
-
-
-
-
-
 });
-
-
 
 //*** Haijin ***************************************************************************************************************************/
 //le Haijin s'exécute dans la div qui lui est dédiée (#haijin)
 const perso = d3.select("#haijinPerso");
-const histoire = d3.select('#histoire'); // pour pouvoir récupérer la position du scroll
+const histoire = d3.select("#histoire"); // pour pouvoir récupérer la position du scroll
 
 histoire.on("scroll", function () {
   // vérifie si le scroll a atteint la fin de la div "histoire"
@@ -329,8 +387,8 @@ histoire.on("scroll", function () {
         // Ajout de la translation dans la matrice de transformation existante (HTML)
         return d3.interpolateString(
           "matrix(0.6,0,0,0.6,0,-10)", // transformation initiale
-          "matrix(0.6,0,0,0.6,0,0)"    // transformation finale (sans la translation)
-        )
+          "matrix(0.6,0,0,0.6,0,0)" // transformation finale (sans la translation)
+        );
       })
       .on("end", stopHaijin); // appeller fonction stop quand fini
   }
@@ -344,9 +402,9 @@ histoire.on("scroll", function () {
         // Ajout de la translation dans la matrice de transformation existante (HTML)
         return d3.interpolateString(
           "matrix(0.6,0,0,0.6,0,0)", // transformation initiale
-          "matrix(0.6,0,0,0.6,0,0)"    // transformation finale (sans la translation)
-        )
-      })
+          "matrix(0.6,0,0,0.6,0,0)" // transformation finale (sans la translation)
+        );
+      });
   }
 
   // Si il y a un scroll, on appelle la fonction moveHaijin sinon on appelle la fonction stopHaijin
@@ -355,5 +413,4 @@ histoire.on("scroll", function () {
   } else {
     stopHaijin();
   }
-
 });
